bcab0859458f1c8fae41847fd3e43e8d7f4f67fd
Alex Rebouças De Carvalho <alex.r.de.carvalho@avanade.com>
2026-06-01 16:36:10 -0300

    Fix Microsoft login with MSAL

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -85,13 +85,16 @@ def authorized():
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
-        # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-        result = None
+        result = _build_msal_app(cache=cache).acquire_token_by_auth_code_flow(
+            session.get("flow", {}),
+            request.args,
+            redirect_uri=url_for("authorized", _external=True)
+        )
         if "error" in result:
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
-        # Note: In a real app, we'd use the 'name' property from session["user"] below
-        # Here, we'll use the admin username for anyone who is authenticated by MS
+        # Aqui mantém a regra do projeto:
+        # qualquer login Microsoft entra como admin
         user = User.query.filter_by(username="admin").first()
         login_user(user)
         _save_cache(cache)
@@ -110,19 +113,33 @@ def logout():
 
     return redirect(url_for('login'))
 
+
 def _load_cache():
-    # TODO: Load the cache from `msal`, if it exists
-    cache = None
+    cache = msal.SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
     return cache
 
 def _save_cache(cache):
-    # TODO: Save the cache, if it has changed
-    pass
+    if cache and cache.has_state_changed:
+        session["token_cache"] = cache.serialize()
 
 def _build_msal_app(cache=None, authority=None):
-    # TODO: Return a ConfidentialClientApplication
-    return None
+    return msal.ConfidentialClientApplication(
+        app.config["CLIENT_ID"],
+        authority=authority or Config.AUTHORITY,
+        client_credential=app.config["CLIENT_SECRET"],
+        token_cache=cache
+    )
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return None
+    flow = _build_msal_app(
+        authority=authority
+    ).initiate_auth_code_flow(
+        scopes=scopes or [],
+        state=state,
+        redirect_uri=url_for("authorized", _external=True)
+    )
+    session["flow"] = flow
+    return flow["auth_uri"]
+
